22b7f82b2f83e7eb426c3bca70499b1a3ed017a8
OferGM <ofer.graber@gmail.com>
2024-12-23 02:34:26 +0200

    iteration 1 done
    finished the fucking models and placed all the lights

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -8,13 +8,13 @@ app = Ursina()
 random.seed(0)
 Entity.default_shader = lit_with_shadows_shader
 
-ground = Entity(model='plane', collider='mesh', scale=(2500, 0, 2500), texture='grass')
+ground = Entity(model='plane', collider='mesh', scale=(974, 0, 1221), texture='grass')
 
 editor_camera = EditorCamera(enabled=True, ignore_paused=True)
 player = FirstPersonController(model='cube', z=-10, color=color.orange, origin_y=-.5, speed=8, collider='box')
 player.speed = 100
 
-roads = Entity(model='resources\\3d_models\\roads.gltf', origin_y=-.5, scale=2,
+roads = Entity(model='resources\\3d_models\\new_roads.glb', origin_y=-.5, origin_x=-.25, origin_z=-1.5, scale=2,
                collider='box',
                shader=lit_with_shadows_shader
                )
@@ -22,19 +22,18 @@ roads = Entity(model='resources\\3d_models\\roads.gltf', origin_y=-.5, scale=2,
 # Lightning for better visibility
 DirectionalLight(y=1.525, z=0, shadows=True)
 
-Entity(model='resources\\3d_models\\e30.glb', origin_y=-.5, scale=5,
-       position=(-85.67, 0.7, 56.99),
-       rotation_y=270,
-       collider='box'
-       )
+car = Entity(model='resources\\3d_models\\e30.glb', origin_y=-.5, scale=5,
+             position=(-77.85, 0.7, 64.05),
+             rotation_y=270,
+             collider='box'
+             )
 
-
-Entity(model='resources\\3d_models\\cst.glb', origin_y=-.5, scale=0.05,
-       position=(-74.27, 1.24, 51.96),
-       rotation_y=180,
-       rotation_x=270,
-       collider='box'
-       )
+passenger = Entity(model='resources\\3d_models\\cst.glb', origin_y=-.5, scale=0.05,
+                   position=(-74.40, 1.24, 55.20),
+                   rotation_y=180,
+                   rotation_x=270,
+                   collider='box'
+                   )
 
 
 # Traffic light system
@@ -114,27 +113,63 @@ class TrafficLight:
 
 
 # Create traffic lights
-traffic_light = TrafficLight(position=(-52.7, -3.75, 62.18), rotation_y=180)
-traffic_light2 = TrafficLight(position=(-72.45, -3.75, 52.45), rotation_y=270)
-traffic_light3 = TrafficLight(position=(-72.45, -3.75, 71.90), rotation_y=90)
+traffic_light1 = TrafficLight(position=(-52.40, -3.75, 65.20), rotation_y=180)
+traffic_light2 = TrafficLight(position=(-71.90, -3.75, 75.00), rotation_y=90)
+traffic_light3 = TrafficLight(position=(-71.90, -3.75, 55.50), rotation_y=270)
 
-traffic_light4 = TrafficLight(position=(-72.45, -3.75, 189.44), rotation_y=0)
-traffic_light5 = TrafficLight(position=(-52.7, -3.75, 199.40), rotation_y=90)
-traffic_light6 = TrafficLight(position=(-52.7, -3.75, 179.50), rotation_y=270)
+traffic_light4 = TrafficLight(position=(-71.90, -3.75, 192.50), rotation_y=0)
+traffic_light5 = TrafficLight(position=(-52.40, -3.75, 202.15), rotation_y=90)
+traffic_light6 = TrafficLight(position=(-52.40, -3.75, 182.80), rotation_y=270)
 
-traffic_light7 = TrafficLight(position=(-52.7, -3.75, -65.28), rotation_y=180)
-traffic_light8 = TrafficLight(position=(-72.45, -3.75, -75.00), rotation_y=270)
-traffic_light9 = TrafficLight(position=(-72.45, -3.75, -55.50), rotation_y=90)
+traffic_light7 = TrafficLight(position=(-52.40, -3.75, -62.30), rotation_y=180)
+traffic_light8 = TrafficLight(position=(-71.90, -3.75, -72.10), rotation_y=270)
+traffic_light9 = TrafficLight(position=(-71.90, -3.75, -52.50), rotation_y=90)
 
-traffic_light10 = TrafficLight(position=(-72.45, -3.75, -193.89), rotation_y=0)
-traffic_light11 = TrafficLight(position=(-52.7, -3.75, -203.7), rotation_y=270)
-traffic_light12 = TrafficLight(position=(-52.7, -3.75, -183.7), rotation_y=270)
+traffic_light10 = TrafficLight(position=(-71.90, -3.75, -190.80), rotation_y=0)
+traffic_light11 = TrafficLight(position=(-52.40, -3.75, -200.65), rotation_y=270)
+traffic_light12 = TrafficLight(position=(-52.40, -3.75, -181.00), rotation_y=90)
+
+traffic_light13 = TrafficLight(position=(-198.60, -3.75, -301.35), rotation_y=0)
+traffic_light14 = TrafficLight(position=(-198.60, -3.75, -320.75), rotation_y=270)
+traffic_light15 = TrafficLight(position=(-179.15, -3.75, -320.75), rotation_y=180)
+traffic_light16 = TrafficLight(position=(-179.15, -3.75, -301.35), rotation_y=90)
+
+traffic_light17 = TrafficLight(position=(-198.40, -3.75, -52.55), rotation_y=0)
+traffic_light18 = TrafficLight(position=(-198.40, -3.75, -72.07), rotation_y=270)
+traffic_light19 = TrafficLight(position=(-178.98, -3.75, -72.07), rotation_y=180)
+traffic_light20 = TrafficLight(position=(-178.98, -3.75, -52.55), rotation_y=90)
+
+traffic_light21 = TrafficLight(position=(-198.45, -3.75, 74.80), rotation_y=0)
+traffic_light22 = TrafficLight(position=(-198.45, -3.75, 55.39), rotation_y=270)
+traffic_light23 = TrafficLight(position=(-179.05, -3.75, 55.39), rotation_y=180)
+traffic_light24 = TrafficLight(position=(-179.05, -3.75, 74.80), rotation_y=90)
+
+traffic_light25 = TrafficLight(position=(-198.45, -3.75, 326.45), rotation_y=0)
+traffic_light26 = TrafficLight(position=(-198.45, -3.75, 306.92), rotation_y=270)
+traffic_light27 = TrafficLight(position=(-178.90, -3.75, 306.92), rotation_y=180)
+traffic_light28 = TrafficLight(position=(-178.90, -3.75, 326.45), rotation_y=90)
+
+traffic_light29 = TrafficLight(position=(-305.82, -3.75, 192.35), rotation_y=180)
+traffic_light30 = TrafficLight(position=(-325.33, -3.75, 202.30), rotation_y=90)
+traffic_light31 = TrafficLight(position=(-325.33, -3.75, 182.65), rotation_y=270)
+
+traffic_light32 = TrafficLight(position=(-325.33, -3.75, 65.10), rotation_y=0)
+traffic_light33 = TrafficLight(position=(-305.82, -3.75, 74.75), rotation_y=90)
+traffic_light34 = TrafficLight(position=(-305.82, -3.75, 55.30), rotation_y=270)
+
+traffic_light35 = TrafficLight(position=(-325.33, -3.75, -62.45), rotation_y=0)
+traffic_light36 = TrafficLight(position=(-305.82, -3.75, -52.70), rotation_y=90)
+traffic_light37 = TrafficLight(position=(-305.82, -3.75, -72.10), rotation_y=270)
+
+traffic_light38 = TrafficLight(position=(-305.82, -3.75, -190.80), rotation_y=180)
+traffic_light39 = TrafficLight(position=(-325.33, -3.75, -181.20), rotation_y=90)
+traffic_light40 = TrafficLight(position=(-325.33, -3.75, -200.60), rotation_y=270)
 
 
 def update():
     # Update traffic light state
-    if traffic_light:
-        traffic_light.update()
+    if traffic_light1:
+        traffic_light1.update()
         traffic_light2.update()
         traffic_light3.update()
 
@@ -146,7 +181,7 @@ def input(key):
 
     # If right-clicked, switch traffic light to green
     if held_keys['right mouse']:
-        traffic_light.switch_to_green()
+        traffic_light1.switch_to_green()
         traffic_light2.switch_to_green()
         traffic_light3.switch_to_green()
 
